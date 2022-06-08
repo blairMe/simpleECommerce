d3e353c7048f8cc82e48457ea89b6378c1b40c98
blairMe <felixanachoni@gmail.com>
2022-06-08 13:22:23 +0300

    EShop

--- src/common/header/Head.jsx
+++ src/common/header/Head.jsx
@@ -7,7 +7,14 @@ const Head = () => {
         <div className="container d_flex">
           <div className="left row">
             <i className="fa fa-phone"></i>
-            <label htmlFor=""></label>
+            <label>+254 710000000</label>
+            <i className="fa fa-envelope"></i>
+            <label>example@gmail.com</label>
+          </div>
+          <div className="right row RText">
+            <label>Theme FAQ's</label>
+            <label>Needs Help</label>
+            <span></span>
           </div>
         </div>
       </section>
