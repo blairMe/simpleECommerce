c5d129e914fefa43def40ba5ff0a1f91c30320e7
blairMe <felixanachoni@gmail.com>
2022-06-09 15:49:56 +0300

    EShop

--- src/common/header/Search.jsx
+++ src/common/header/Search.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import logo from '../assets/images/logo.svg'
+import { Link } from "react-router-dom";
 
 const Search = () => {
   return (
@@ -7,7 +8,22 @@ const Search = () => {
       <section className="search">
         <div className="container c_flex">
           <div className="logo width">
-            <img src={logo} alt='' />
+            <img src={logo} alt="" />
+          </div>
+
+          <div className="search_box f_flex">
+            <i className="fa fa-search"></i>
+            <input type="text" placeholder="Search and hit enter..." />
+            <span>All Categories</span>
+          </div>
+          <div className="icon f_flex width">
+            <i className="fa fa-user icon_circle"></i>
+            <div className="cart">
+              <Link to="/cart">
+                <i className="fa fa-shopping-bag icon-circle"></i>
+                <span>0</span>
+              </Link>
+            </div>
           </div>
         </div>
       </section>
