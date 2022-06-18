fa3b7a9a3dd3ac24cba04029e632775ff87b2fdd
blairMe <felixanachoni@gmail.com>
2022-06-18 22:32:18 +0300

    EShop

--- src/components/flashdeals/FlashCard.jsx
+++ src/components/flashdeals/FlashCard.jsx
@@ -17,7 +17,6 @@ const FlashCard = ({ productItems }) => {
             </div>
             <div className="product-details">
               <h3>{product}</h3>
-              {/* More code coming soon */}
               <div className="rate">
                 <i className="fa fa-star"></i>
                 <i className="fa fa-star"></i>
