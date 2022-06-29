0abc050680ee87cb3f0c939bba796faeae3af50f
blairMe <felixanachoni@gmail.com>
2022-06-29 23:31:08 +0300

    EShop

--- src/components/flashdeals/FlashCard.jsx
+++ src/components/flashdeals/FlashCard.jsx
@@ -23,7 +23,7 @@ const PrevArrow = (props) => {
   );
 };
 
-const FlashCard = ({ productItem, addToCart }) => {
+const FlashCard = ({ productItems, addToCart }) => {
   const [count, setCount] = useState(0);
   const increment = () => {
     setCount(count + 1);
@@ -41,7 +41,7 @@ const FlashCard = ({ productItem, addToCart }) => {
 
   return (
     <>
-      <Slider {...settings}>
+      {/* <Slider {...settings}>
         {productItem.map((productItems) => {
           return (
             <div className="box">
@@ -65,6 +65,43 @@ const FlashCard = ({ productItem, addToCart }) => {
                   </div>
                   <div className="price">
                     <h4>{productItems.price}.00</h4>
+                    <button onClick={() => addToCart(productItems)}>
+                      <i className="fa fa-plus"></i>
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </Slider> */}
+      <Slider {...settings}>
+        {productItems.map((productItems) => {
+          return (
+            <div className="box">
+              <div className="product mtop">
+                <div className="img">
+                  <span className="discount">{productItems.discount}% Off</span>
+                  <img src={productItems.cover} alt="" />
+                  <div className="product-like">
+                    <label>{count}</label> <br />
+                    <i className="fa-regular fa-heart" onClick={increment}></i>
+                  </div>
+                </div>
+                <div className="product-details">
+                  <h3>{productItems.name}</h3>
+                  <div className="rate">
+                    <i className="fa fa-star"></i>
+                    <i className="fa fa-star"></i>
+                    <i className="fa fa-star"></i>
+                    <i className="fa fa-star"></i>
+                    <i className="fa fa-star"></i>
+                  </div>
+                  <div className="price">
+                    <h4>${productItems.price}.00 </h4>
+                    {/* step : 3  
+                     if hami le button ma click garryo bahne 
+                    */}
                     <button onClick={() => addToCart(productItems)}>
                       <i className="fa fa-plus"></i>
                     </button>
