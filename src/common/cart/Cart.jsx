2da0df7b13e66caec96adb79d3968ee5c5463b65
blairMe <felixanachoni@gmail.com>
2022-07-19 23:23:44 +0300

    EShop

--- src/common/cart/Cart.jsx
+++ src/common/cart/Cart.jsx
@@ -1,6 +1,8 @@
 import React from "react";
+import './style.css'
 
-const Cart = ({ cartItem, addToCart }) => {
+const Cart = ({ cartItem, addToCart, decreaseQty }) => {
+  const totalPrice = cartItem.reduce((price, item) => price + item.qty * item.price, 0)
   return (
     <>
       <section className="cart-items">
@@ -11,28 +13,54 @@ const Cart = ({ cartItem, addToCart }) => {
                 No items are added in the Cart
               </h1>
             )}
-          </div>
-          {
-            cartItem.map((item) => {
-              const prodcutQty = item.price * item.qty
-              return(
+            {/* </div> */}
+            {cartItem.map((item) => {
+              const prodcutQty = item.price * item.qty;
+              return (
                 <div className="cartlist product d_flex">
                   <div className="img">
                     <img src={item.cover} alt="" />
                   </div>
                   <div className="cart-details">
                     <h3>{item.name}</h3>
-                    <h4>{item.price} * {item.qty}
-                    <span>${prodcutQty}.00</span>
+                    <h4>
+                      {item.price} * {item.qty}
+                      <span>${prodcutQty}.00</span>
                     </h4>
                   </div>
                   <div className="cart-items-function">
-                    <div className="removeCart"></div>
+                    <div className="removeCart">
+                      <button className="removeCart">
+                        <i className="fa solid fa-xmark"></i>
+                      </button>
+                    </div>
+                    <div className="cartControl d_flex">
+                      <button
+                        className="incCart"
+                        onClick={() => addToCart(item)}
+                      >
+                        <i className="fa fa-plus"></i>
+                      </button>
+                      <button
+                        className="incCart"
+                        onClick={() => decreaseQty(item)}
+                      >
+                        <i className="fa fa-minus"></i>
+                      </button>
+                    </div>
                   </div>
+                  <div className="cart-item-price"></div>
                 </div>
-              )
-            })
-          }
+              );
+            })}
+          </div>
+          <div className="cart-total product">
+            <h2>Cart Summary</h2>
+            <div className="d_flex">
+              <h4>Total price: </h4>
+              <h3>{totalPrice}.00</h3>
+            </div>
+          </div>
         </div>
       </section>
     </>
